18cb27b5e4121bb1c988860546c88169b830c126
bairock <bairock@live.ru>
2021-07-30 16:20:02 +0900

    add lib

---
+++ lib/index.d.ts
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react';
+export declare function isPortalExisted(name: string): boolean;
+export interface PortalManager {
+    update: (updater: React.ReactNode, updateCallback?: (() => void) | undefined) => void;
+    destroy: (destroyCallback?: () => void) => void;
+}
+export declare function enterPortal(target: string, guest: ReactNode, callback?: () => void): PortalManager;
+export declare function PortalEntry(props: {
+    children: ReactNode;
+    target?: string;
+}): JSX.Element | null;
+export declare function PortalExit(props: {
+    name: string;
+    renderSibling?: (sibling: ReactNode) => ReactNode;
+    children?: ReactNode;
+}): JSX.Element;
+declare const _default: {
+    Entry: typeof PortalEntry;
+    Exit: typeof PortalExit;
+    isExisted: typeof isPortalExisted;
+    enter: typeof enterPortal;
+};
+export default _default;
